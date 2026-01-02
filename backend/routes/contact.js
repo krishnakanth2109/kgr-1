c5c68b5a46a56f512df18d2a4990866a73f7c5cd
Krishna kanth <146426157+krishnakanth2109@users.noreply.github.com>
2026-01-02 13:26:29 +0530

    Update contact.js

--- backend/routes/contact.js
+++ backend/routes/contact.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const router = express.Router();
 const Contact = require('../models/contact');
-const Notification = require('../models/Notification'); // Import Notification Model
+const Notification = require('../models/notification'); // Import Notification Model
 
 // GET all messages
 router.get('/', async (req, res) => {
